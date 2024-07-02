da5207a7107c29dd4257b66d82c8a8c7a4fd3958
Rosemberg Zambrano García <zaar1387@gmail.com>
2024-07-01 19:18:20 -0500

    ajustando wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 from waitress import serve
-from main import app
+from main import main
 
 #python wsgi.py
 
 
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=5000)
+    serve(main, host='0.0.0.0', port=5000)
